24dac96f9c444e537f7f6b57017ccb2222be9d50
walksanatora <walkerffo22@gmail.com>
2023-08-24 14:23:30 -0700

    working c lib?

--- sam/lib.c
+++ sam/lib.c
@@ -6,5 +6,24 @@
 #include "reciter.h"
 #include "sam.h"
 #include "debug.h"
+#include "lib.h"
 
 int debug = 0;
+
+struct AudioResult speakText(unsigned char pitch,unsigned char speed,unsigned char throat,unsigned char mouth,char *input)
+{
+    SetPitch(pitch == 0 ? 64 : pitch);
+    SetSpeed(speed == 0 ? 72 : speed);
+    SetThroat(throat == 0 ? 128 :throat);
+    SetMouth(mouth == 0 ? 128 : mouth);
+    SetInput(input);
+    int res = SAMMain();
+    char *buf = GetBuffer();
+    int buf_size = GetBufferLength();
+    struct AudioResult resp;
+    resp.res = res;
+    resp.buf = buf;
+    resp.buf_size = buf_size;
+    return resp;
+}
+
--- sam/lib.h
+++ sam/lib.h
@@ -1,3 +1,13 @@
 
 #include "reciter.h"
 #include "sam.h"
+
+int debug;
+
+struct AudioResult {
+    int res;
+    int buf_size;
+    char *buf;
+};
+
+struct AudioResult speakText(unsigned char pitch,unsigned char speed,unsigned char throat,unsigned char mouth,char *input);
